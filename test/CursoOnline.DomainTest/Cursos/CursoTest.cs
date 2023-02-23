42b01e8fef696ef7ab3c2aa01a58d17042d3bd9a
Rodrigo <rodrigoalves_@outlook.com>
2023-02-22 23:32:46 -0300

    Realizando testes de erros com exceptions

--- test/CursoOnline.DomainTest/Cursos/CursoTest.cs
+++ test/CursoOnline.DomainTest/Cursos/CursoTest.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.VisualStudio.TestPlatform.Common.Telemetry;
+﻿using CursoOnline.DomainTest._Utils;
+using ExpectedObjects;
+using Microsoft.VisualStudio.TestPlatform.Common.Telemetry;
 using System;
 using System.Collections.Generic;
 using System.Drawing;
@@ -13,33 +15,107 @@ namespace CursoOnline.DomainTest.Cursos
         [Fact]
         public void DeveCriarCurso()
         {
-            const string nome = "Informática Básica";
-            const double cargaHoraria = 80;
-            const string publicoAlvo = "Estudantes";
-            const double valor = 950;
+            var cursoEsperado = new
+            {
+                Nome = "Informática Básica",
+                CargaHoraria = (double)80,
+                PublicoAlvo = PublicoAlvo.Estudante,
+                Valor = (double)950,
+            };
 
-            var curso = new Curso(nome, cargaHoraria, publicoAlvo, valor);
+            var curso = new Curso(cursoEsperado.Nome, cursoEsperado.CargaHoraria, cursoEsperado.PublicoAlvo, cursoEsperado.Valor);
 
-            Assert.Equal(nome, curso.Nome);
-            Assert.Equal(cargaHoraria, curso.CargaHoraria);
-            Assert.Equal(publicoAlvo, curso.PublicoAlvo);
-            Assert.Equal(valor, curso.Valor);
+            cursoEsperado.ToExpectedObject().ShouldMatch(curso);
         }
+
+        [Theory]
+        [InlineData("")]
+        [InlineData(null)]
+        public void NaoDeveCursoTerUmNomeInvalido(string nomeInvalido)
+        {
+            var cursoEsperado = new
+            {
+                Nome = "Informática Básica",
+                CargaHoraria = (double)80,
+                PublicoAlvo = PublicoAlvo.Estudante,
+                Valor = (double)950,
+            };
+
+            // Maneira nao otimizada para realizar o assert da exception.
+            // var message = Assert.Throws<ArgumentException>(() => new Curso(nomeInvalido, cursoEsperado.CargaHoraria, cursoEsperado.PublicoAlvo, cursoEsperado.Valor)).Message;
+            // Assert.Equal("Nome inválido", message);
+
+            Assert.Throws<ArgumentException>(() => new Curso(nomeInvalido, cursoEsperado.CargaHoraria, cursoEsperado.PublicoAlvo, cursoEsperado.Valor))
+                .ComMensagem("Nome inválido");
+        }
+
+
+        [Theory]
+        [InlineData(0)]
+        [InlineData(-12)]
+        public void NaoDeveCursoTerCargaHorariaMenosQueUm(double cargaHorariaInvalida)
+        {
+            var cursoEsperado = new
+            {
+                Nome = "Informática Básica",
+                CargaHoraria = (double)80,
+                PublicoAlvo = PublicoAlvo.Estudante,
+                Valor = (double)950,
+            };
+
+            Assert.Throws<ArgumentException>(() => new Curso(cursoEsperado.Nome, cargaHorariaInvalida, cursoEsperado.PublicoAlvo, cursoEsperado.Valor))
+                .ComMensagem("Carga Horaria Inválida");
+        }
+
+        [Theory]
+        [InlineData(0)]
+        [InlineData(-12)]
+        public void NaoDeveCursoTerValorMenosQueUm(double valorInvalida)
+        {
+            var cursoEsperado = new
+            {
+                Nome = "Informática Básica",
+                CargaHoraria = (double)80,
+                PublicoAlvo = PublicoAlvo.Estudante,
+                Valor = (double)950,
+            };
+
+            Assert.Throws<ArgumentException>(() => new Curso(cursoEsperado.Nome, cursoEsperado.CargaHoraria, cursoEsperado.PublicoAlvo, valorInvalida))
+                .ComMensagem("Valor Inválido");
+        }
+    }
+
+    public enum PublicoAlvo
+    {
+        Estudante,
+        Universitario,
+        Empregado,
+        Empreendedor
     }
 
     public class Curso
     {
         public string Nome { get; private set; }
         public double CargaHoraria { get; private set; }
-        public string PublicoAlvo { get; private set; }
+        public PublicoAlvo PublicoAlvo { get; private set; }
         public double Valor { get; private set; }
 
-        public Curso(string nome, double cargaHoraria, string publicoAlvo, double valor)
+
+        public Curso(string nome, double cargaHoraria, PublicoAlvo publicoAlvo, double valor)
         {
-            this.Nome = nome;
-            this.CargaHoraria = cargaHoraria;
-            this.PublicoAlvo = publicoAlvo;
-            this.Valor = valor;
+            if (string.IsNullOrEmpty(nome))
+                throw new ArgumentException("Nome inválido");
+
+            if (cargaHoraria < 1)
+                throw new ArgumentException("Carga Horaria Inválida");
+
+            if (valor < 1)
+                throw new ArgumentException("Valor Inválido");
+
+            Nome = nome;
+            CargaHoraria = cargaHoraria;
+            PublicoAlvo = publicoAlvo;
+            Valor = valor;
         }
     }
 }
---
+++ test/CursoOnline.DomainTest/_Utils/AssertExtension.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CursoOnline.DomainTest._Utils
+{
+    public static class AssertExtension
+    {
+        public static void ComMensagem(this ArgumentException exception, string mensagem)
+        {
+            if(exception.Message == mensagem)
+            {
+                Assert.True(true);
+            }
+            else
+            {
+                Assert.False(true, $"Não esperava a mensagem `{mensagem}`");
+            }
+        }
+    }
+}
