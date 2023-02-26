9fc68690cfafe6b1378876204af4888727e904f3
Rodrigo <rodrigoalves_@outlook.com>
2023-02-26 18:06:30 -0300

    Melhorando verificações dos mocks

--- test/CursoOnline.DomainTest/Cursos/ArmazenadorCursoTest.cs
+++ test/CursoOnline.DomainTest/Cursos/ArmazenadorCursoTest.cs
@@ -1,29 +1,45 @@
-﻿using CursoOnline.Domain.Cursos;
+﻿using Bogus;
+using CursoOnline.Domain.Cursos;
 using Moq;
 
 namespace CursoOnline.DomainTest.Cursos
 {
     public class ArmazenadorCursoTest
     {
+        private CursoDto _cursoDto;
+        private Mock<ICursoRepositorio> _cursoRepositorioMock;
+        private ArmazenadorCurso _armazenadorCurso;
+
+        public ArmazenadorCursoTest()
+        {
+            var faker = new Faker();
+
+            _cursoDto = new CursoDto
+            {
+                Nome = faker.Random.Word(),
+                Descricao = faker.Lorem.Paragraph(),
+                CargaHoraria = faker.Random.Double(50,100),
+                PublicoAlvo = PublicoAlvo.Empreendedor,
+                Valor = faker.Random.Double(500, 1000)
+            };
+
+            _cursoRepositorioMock = new Mock<ICursoRepositorio>();
+            _armazenadorCurso = new ArmazenadorCurso(_cursoRepositorioMock.Object);
+        }
+
         [Fact]
         public void DeveAdicionarCurso()
         {
-            var cursoDto = new CursoDto
-            {
-                Nome = "Curso A",
-                Descricao = "Descrição A",
-                CargaHoraria = (double)80,
-                PublicoAlvo = PublicoAlvo.Empreendedor,
-                Valor = (double)100
-            };
+            _armazenadorCurso.Armazenar(_cursoDto);
 
-            var cursoRepositorioMock = new Mock<ICursoRepositorio>();
+            // Verifica se o curso foi instanciado
+            //cursoRepositorioMock.Verify(r => r.Adicionar(It.IsAny<Curso>())); 
 
-            var armazenadorCurso = new ArmazenadorCurso(cursoRepositorioMock.Object);
-
-            armazenadorCurso.Armazenar(cursoDto);
-
-            cursoRepositorioMock.Verify(r => r.Adicionar(It.IsAny<Curso>()));
+            // Verifica seo objeto instaciado tem a mesma propriedade que o dto
+            _cursoRepositorioMock.Verify(r => r.Adicionar(
+                    It.Is<Curso>(c => c.Nome == _cursoDto.Nome)
+                ), Times.AtLeast(1) // Verifica quantas vezes foi instanciado o objeto
+            ); 
         }
     }
 
